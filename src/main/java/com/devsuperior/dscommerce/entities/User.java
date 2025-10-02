641206172b24be033cfb9e289de72e74e4fe3fde
NicolasKevinCaetano <nicolaskevinoliveira@gmail.com>
2025-10-02 19:49:07 -0300

    Payment

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -41,6 +41,7 @@ public class User {
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
@@ -49,14 +50,17 @@ public class User {
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
@@ -65,22 +69,27 @@ public class User {
     }
 
     public void setPhone(String phone) {
+
         this.phone = phone;
     }
 
     public LocalDate getBirthDate() {
+
         return birthDate;
     }
 
     public void setBirthDate(LocalDate birthDate) {
+
         this.birthDate = birthDate;
     }
 
     public String getPassword() {
+
         return password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
